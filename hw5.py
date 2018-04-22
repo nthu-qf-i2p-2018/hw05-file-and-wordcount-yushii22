588ff73e82afb48a0eaa21f02dc2c6fa1ec140e3
106071015 <37043933+yushii22@users.noreply.github.com>
2018-04-22 15:30:29 +0800

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,48 +1,49 @@
 # -*- coding: utf-8 -*-
-import ...
 
+import csv
+import json
 
 def main(filename):
     # read file into lines
-    lines = ...
-
+    file = open(filename)
+    lines = file.readlines()
     # declare a word list
     all_words = []
-
     # extract all words from lines
     for line in lines:
         # split a line of text into a list words
         # "I have a dream." => ["I", "have", "a", "dream."]
-        words = ...
-
+        words = line.split()
         # check the format of words and append it to "all_words" list
         for word in words:
             # then, remove (strip) unwanted punctuations from every word
             # "dream." => "dream"
-            word = ...
+            word = word.strip('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
             # check if word is not empty
             if word:
                 # append the word to "all_words" list
-                all_words...
-
+                all_words.append(word)
     # compute word count from all_words
-    counter = ...
+    from collections import Counter
+    counter = Counter(all_words)
 
     # dump to a csv file named "wordcount.csv":
     # word,count
     # a,12345
     # I,23456
     # ...
-    with open(...) as csv_file:
+    with open("wordcount.csv", "w") as csv_file:
         # create a csv writer from a file object (or descriptor)
-        writer = ...
+        writer = csv.writer(csv_file)
         # write table head
-        writer.writerow(['word', 'count'])
+        writer.writerow(['word','count'])
         # write all (word, count) pair into the csv writer
-        writer.writerows(...)
+        writer.writerows(counter.most_common())
 
     # dump to a json file named "wordcount.json"
-    ...
+    f = open("wordcount.json", 'w')
+    json.dump(counter.most_common(), f)
+   
 
     # BONUS: dump to a pickle file named "wordcount.pkl"
     # hint: dump the Counter object directly
